7a766a7878c1edfcfcf4849d54a9da279655599d
Okeomasilachi <okeomasilachi@gmail.com>
2023-05-16 12:52:30 +0100

    solve error for scripts

--- main.c
+++ main.c
@@ -8,41 +8,8 @@ int main(int argc, char **argv)
     int i;
     
     (void)n;
-    /* Check if av[1] is a file */	
-    if (argc <= 1)
+    if (argc > 1)
     {
-        /*Interactive mode*/
-        while (true)
-        {
-            char **command = NULL;
-            
-            pr = "$ ";
-            printf("%s", pr);
-            br = getline(&cmd, &n, stdin);
-            if (br == EOF) 
-                break;
-
-            if (cmd[0] == '\n') continue;
-            command = prs(cmd, 1);
-            
-            for (i = 0; command[i] != NULL; i++)
-            {
-                av = prs(command[i], 0);
-                if (!execute_builtin_command(av, Name, argc))
-                {
-                    execute_command(av, environ, 0, Name, argc);
-                }
-            }
-            for (i = 0; av[i] != NULL; i++)
-                    free(av[i]);
-
-            free(av);
-        }
-            
-    }
-    else if (argc > 1)
-    {
-        pr = NULL;
 	    if (argc == 2 && access(argv[1], F_OK) == 0 && access(argv[1], X_OK) != 0)
 	    {
 	            file_process(argv, av, cmd, Name, argc);
@@ -61,6 +28,31 @@ int main(int argc, char **argv)
             exec_command(args, environ, com, Name, argc);
             
 	    }
+    }
+
+    while (true)
+    {
+        char **command = NULL;
+        
+        pr = "$ ";
+        printf("%s", pr);
+        br = getline(&cmd, &n, stdin);
+        if (br == EOF) 
+            break;
+        if (cmd[0] == '\n') continue;
+        command = prs(cmd, 1);
+        
+        for (i = 0; command[i] != NULL; i++)
+        {
+            av = prs(command[i], 0);
+            if (!execute_builtin_command(av, Name, argc))
+            {
+                execute_command(av, environ, 0, Name, argc);
+            }
+        }
+        for (i = 0; av[i] != NULL; i++)
+                free(av[i]);
+        free(av);
     }
     free(cmd);
     putchar('\n'); 
