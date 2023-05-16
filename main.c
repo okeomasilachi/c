0e52abcc1b5c433df9217b4abd64c3d7387b9ee1
Okeomasilachi <okeomasilachi@gmail.com>
2023-05-16 16:54:57 +0100

    okok

--- main.c
+++ main.c
@@ -7,38 +7,14 @@ int main(int argc, char **argv)
     ssize_t br = 0;
     int i;
 
-    if (argc > 1)
+    if (argc == 2 && access(argv[1], F_OK) == 0 && access(argv[1], X_OK) != 0)
     {
-	if (argc == 2 && access(argv[1], F_OK) == 0 && access(argv[1], X_OK) != 0)
-	        file_process(argv, av, cmd, Name, argc);
-	else
-	{
-		if (argv)
-		{
-			for (i = 1; i < argc; i++)
-		 	   cmd_length += strlen(argv[i]) + 1; /* +1 for space separator */
-			cmd = (char*)malloc(cmd_length + 1); /* +1 for null terminator */
-			cmd[0] = '\0'; /* Ensuring the string is empty before concatenating */
-			for (i = 1; i < argc; i++) {
-				strcat(cmd, argv[i]);
-				strcat(cmd, " ");
-			}		
-		}
-		command = prs(cmd, 1);
-		av = prs(command[i], 0);
-		if (!execute_builtin_command(av,Name,argc))
-		{
-			char *ec;
-			if ((ec = find_executable(av[0])) != NULL)
-			{
-				execve(ec, av, environ);
-			}
-		}
-	}
+	file_process(argv, av, cmd, Name, argc);
     }
-    else
+
+    if (argc == 1 && argv[1] == NULL)
     {
-	while (cmd_length == 0)
+	while (true)
 	{
 		write(STDOUT_FILENO, "$ ", 2);
 	        br = getline(&cmd, &n, stdin);
@@ -48,7 +24,28 @@ int main(int argc, char **argv)
 	        command = prs(cmd, 1);
 		B_exc(argc, Name, command, av, environ);
 	}
+	free(cmd);
+	exit(EXIT_SUCCESS);
     }
-    free(cmd);
-    return EXIT_SUCCESS;
+    
+
+	for (i = 1; i < argc; i++)
+		cmd_length += strlen(argv[i]) + 1; /* +1 for space separator */
+	cmd = (char*)malloc(cmd_length + 1); /* +1 for null terminator */
+	cmd[0] = '\0'; /* Ensuring the string is empty before concatenating */
+	for (i = 1; i < argc; i++) {
+		strcat(cmd, argv[i]);
+		strcat(cmd, " ");
+	}		
+	command = prs(cmd, 1);
+	av = prs(command[i], 0);
+	if (!execute_builtin_command(av,Name,argc))
+	{
+		char *ec;
+		if ((ec = find_executable(av[0])) != NULL)
+		{
+			execve("/bin/sh", av, environ);
+		}
+	}
+	return 0;
 }
